fdd427d72dd8143ed9cea4290c2e2ec07ac32d57
4Cross <105809123+4CROS2@users.noreply.github.com>
2024-03-11 03:46:50 -0500

    new dependecies

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+// svelte.config.js
+import adapter from 'sveltekit-adapter-html-like';
 
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			fallback: '200.html'
 		})
 	}
 };
